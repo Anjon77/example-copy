5ccebc42d5114340455b023a3969afbe76a330a8
Anjon77 <50800617+Anjon77@users.noreply.github.com>
2023-09-23 04:16:01 +0600

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,68 @@
+
+    <?php 
+        // $name = "Anjon";
+        // echo $name;
+        // echo "\n";
+        // $name = "Ranjon";
+        // $question ="How are you?";
+        // echo $name;
+        // $countryName ="Bangladehs";
+        // echo "hello ".$countryName;
+        // echo "\n";
+        // echo "Hello $countryName";
+        // echo "\n";
+        // echo "Hello {$countryName}";
+        // echo "\n";
+        // echo "Hello {$question} {$countryName}";
+        // echo "\n";
+        // echo "Hello {$countryName}, {$question}";
+
+        define('NAME', "anjon");
+
+        echo NAME;
+        echo "\n";
+        echo constant('NAME');
+
+        $price = 30.3;
+        echo "Dollar value is $$price";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
